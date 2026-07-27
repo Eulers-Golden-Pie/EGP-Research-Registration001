daaf641a110516cd1dc1dc51398d048e03a6c22e
Euler's Golden Pie <eulergoldpi@gmail.com>
2026-07-26 20:56:18 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.APP_CONFIG = Object.freeze({
   appsScriptUrl:
-    "https://script.google.com/macros/s/PASTE_YOUR_DEPLOYMENT_ID/exec"
+    "https://script.google.com/macros/s/AKfycbykStUjwVZAMl5z_5GvtZh9beXQJiNX_F6jhqoql-zjvAZnGzm3_aAEMCi8VyHrD67v/exec"
 });
