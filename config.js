41f29d53c22b5316aec5a9b6aed02041c2ed50cc
Euler's Golden Pie <eulergoldpi@gmail.com>
2026-08-05 10:13:06 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,6 @@
 window.APP_CONFIG = Object.freeze({
   appsScriptUrl:
     "https://script.google.com/macros/s/AKfycbxGXDNiTVhhpmdIvRAVO-YLvli-tV1hSBdfipdMzuZDa_SGEv_hHpD7QJsRiU4GgsezFA/exec"
+  registrationDeadline: "2026-08-05T17:00:00-05:00",
+  maxParticipants: 12
 });
